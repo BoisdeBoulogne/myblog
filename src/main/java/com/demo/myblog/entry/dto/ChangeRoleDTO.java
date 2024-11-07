8eb0b4a343e2a3022bdf439063f40b3d186b7cfc
snow <doufu6640@gmail.com>
2024-11-07 18:20:18 +0800

    MD文档

--- src/main/java/com/demo/myblog/entry/dto/ChangeRoleDTO.java
+++ src/main/java/com/demo/myblog/entry/dto/ChangeRoleDTO.java
@@ -2,10 +2,14 @@ package com.demo.myblog.entry.dto;
 
 import com.demo.myblog.constant.Constants;
 import com.demo.myblog.enums.RoleEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ChangeRoleDTO {
+    @NotNull(message = "用户名不能为空")
     private Integer userId;
+    @NotNull(message = "修改的权限不能为空")
     private RoleEnum roleName;
 }
